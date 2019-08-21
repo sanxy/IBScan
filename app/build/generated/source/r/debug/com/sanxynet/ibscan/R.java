62ca173645d3828f52058ae2c9c608b59c7301e9
sanxy <yusufadefolahan@gmail.com>
2019-08-21 08:00:27 +0100

    added legacy icon, default AS icon

--- app/build/generated/source/r/debug/com/sanxynet/ibscan/R.java
+++ app/build/generated/source/r/debug/com/sanxynet/ibscan/R.java
@@ -9,8 +9,9 @@ package com.sanxynet.ibscan;
 
 public final class R {
   public static final class drawable {
-    public static final int icon=0x7f010000;
-    public static final int logo=0x7f010001;
+    public static final int ic_launcher_background=0x7f010001;
+    public static final int ic_launcher_foreground=0x7f010002;
+    public static final int icon=0x7f010003;
   }
   public static final class id {
     public static final int btnClose=0x7f020000;
@@ -47,37 +48,41 @@ public final class R {
   public static final class layout {
     public static final int enlarged=0x7f030000;
     public static final int file_name_dialog=0x7f030001;
-    public static final int ib_scan_land=0x7f030002;
-    public static final int ib_scan_port=0x7f030003;
+    public static final int ib_scan_activity=0x7f030002;
+    public static final int ib_scan_activity_land=0x7f030003;
     public static final int spinner_text_layout=0x7f030004;
   }
   public static final class menu {
     public static final int scanimage_menu=0x7f040000;
   }
+  public static final class mipmap {
+    public static final int ic_launcher=0x7f050000;
+    public static final int ic_launcher_round=0x7f050001;
+  }
   public static final class string {
-    public static final int app_name=0x7f050000;
-    public static final int capture_seq_name=0x7f050001;
-    public static final int close_scanner_btn=0x7f050002;
-    public static final int device_count_default=0x7f050003;
-    public static final int device_count_name=0x7f050004;
-    public static final int email_image=0x7f050005;
-    public static final int enlarge=0x7f050006;
-    public static final int frame_time_default=0x7f050007;
-    public static final int frame_time_name=0x7f050008;
-    public static final int logo_img=0x7f050009;
-    public static final int open_scanner_btn=0x7f05000a;
-    public static final int preview_image=0x7f05000b;
-    public static final int refresh_btn=0x7f05000c;
-    public static final int start_capture_btn=0x7f05000d;
-    public static final int status_default=0x7f05000e;
-    public static final int status_name=0x7f05000f;
-    public static final int stop_capture_btn=0x7f050010;
-    public static final int usb_device_name=0x7f050011;
+    public static final int app_name=0x7f060000;
+    public static final int capture_seq_name=0x7f060001;
+    public static final int close_scanner_btn=0x7f060002;
+    public static final int device_count_default=0x7f060003;
+    public static final int device_count_name=0x7f060004;
+    public static final int email_image=0x7f060005;
+    public static final int enlarge=0x7f060006;
+    public static final int frame_time_default=0x7f060007;
+    public static final int frame_time_name=0x7f060008;
+    public static final int logo_img=0x7f060009;
+    public static final int open_scanner_btn=0x7f06000a;
+    public static final int preview_image=0x7f06000b;
+    public static final int refresh_btn=0x7f06000c;
+    public static final int start_capture_btn=0x7f06000d;
+    public static final int status_default=0x7f06000e;
+    public static final int status_name=0x7f06000f;
+    public static final int stop_capture_btn=0x7f060010;
+    public static final int usb_device_name=0x7f060011;
   }
   public static final class style {
-    public static final int Enlarged=0x7f060000;
+    public static final int Enlarged=0x7f070000;
   }
   public static final class xml {
-    public static final int device_filter=0x7f070000;
+    public static final int device_filter=0x7f080000;
   }
 }
